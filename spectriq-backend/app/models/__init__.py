2ce8ac59731b82d69a372677399da23d17ffb963
Sargam <sargamshabina06@gmail.com>
2026-07-17 03:27:59 +0530

    Delete spectriq-backend directory

--- spectriq-backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.meeting import Meeting
-from app.models.job import Job
-from app.models.transcript_chunk import TranscriptChunk
-
-__all__ = ["User", "Meeting", "Job", "TranscriptChunk"]
